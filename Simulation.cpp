498f0b726f7b036e2b5c0c4ee6f32919280f6a05
Tobias Mueller <Tobias_Mueller@twam.info>
2011-09-01 12:23:34 +0200

    added Simulation.*

---
+++ Simulation.cpp
@@ -0,0 +1,16 @@
+#include "Simulation.h"
+#include "config.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+Simulation::Simulation()
+{
+}
+
+Simulation::~Simulation()
+{
+}
+
---
+++ Simulation.h
@@ -0,0 +1,48 @@
+#ifndef _SIMULATION_H_
+#define _SIMULATION_H_
+
+#include <QObject>
+
+class Simulation : public QObject
+{
+	Q_OBJECT
+
+	public:
+
+		enum QuantityType {
+			DENSITY,
+			TEMPERATURE,
+			V_RADIAL,
+			V_AZIMUTHAL,
+			N_QUANTITY_TYPES
+		};
+
+		Simulation();
+		virtual ~Simulation();
+
+		virtual int loadFromFile(const char* filename) = 0;
+		virtual unsigned int getNumberOfPlanets() const = 0;
+		virtual const double* getPlanetPosition(unsigned int number) const = 0;
+		virtual const double* getPlanetVelocity(unsigned int number) const = 0;
+		virtual const double* getPlanetMass(unsigned int number) const = 0;
+		virtual int loadTimestep(unsigned int timestep) = 0;
+		virtual unsigned int getCurrentTimestep() const = 0;
+		virtual unsigned int getLastTimeStep() const = 0;
+		virtual unsigned int getNRadial() const = 0;
+		virtual unsigned int getNAzimuthal() const = 0;
+		virtual double getRMin() const = 0;
+		virtual double getRMax() const = 0;
+		virtual const double* getRadii() const = 0;
+		virtual const double* getQuantity() const = 0;
+		virtual void setQuantityType(QuantityType type) = 0;
+
+		virtual double getMinimumValue(void) const = 0;
+		virtual double getMaximumValue(void) const = 0;
+
+	private:
+
+	signals:
+		void dataUpdated();
+};
+
+#endif
